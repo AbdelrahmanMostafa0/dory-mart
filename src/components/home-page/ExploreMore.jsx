fc39c6e69ea60ab1c62ed1e3bfd30317b6a5b47d
abdelrahmanmostafa0 <abdelrahmanmostafa.developer@gmail.com>
2025-02-13 17:31:18 +0200

    style: footer & explore more

--- src/components/home-page/ExploreMore.jsx
+++ src/components/home-page/ExploreMore.jsx
@@ -5,7 +5,8 @@ const ExploreMore = () => {
   return (
     <div className=" py-5 sm:py-20   flex items-center flex-col justify-center w-full mx-auto relative max-w-screen">
       <h3 className="text-center font-zentry text-4xl md:text-8xl font-bold">
-        Explore the Depths of <br /> <mark>Dorymart</mark>
+        Explore the Depths of <br />{" "}
+        <span className="bg-blue-500 px-3">Dorymart</span>
       </h3>
       {/* <div className="flex mt-5 items-center justify-center">
           <Button size="lg">Explore Now</Button>
--- src/components/layout-components/Footer.jsx
+++ src/components/layout-components/Footer.jsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer class="bg-white rounded-lg shadow-sm dark:bg-gray-900 m-4">
+    <footer class="bg-white rounded-lg   dark:bg-gray-900 ">
       <div class="w-full mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
           <Link
             href="/"
-            class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+            class="flex items-center mb-4 sm:mb-0 space-x-2 rtl:space-x-reverse"
           >
             <img src="/dory-logo.png" class="h-8" alt="Flowbite Logo" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              DoryMart
+              Dorymart
             </span>
           </Link>
           <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
@@ -41,7 +41,7 @@ const Footer = () => {
         <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2025{" "}
           <Link href="/" class="hover:underline">
-            DoryMart™
+            Dorymart™
           </Link>
           . All Rights Reserved.
         </span>
