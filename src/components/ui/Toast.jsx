67d664e0f26a72f3d930422db381e598695da3c4
abdelrahmanmostafa0 <abdelrahmanmostafa.developer@gmail.com>
2025-03-12 11:16:47 +0200

    style: fix toast style

--- src/components/ui/Toast.jsx
+++ src/components/ui/Toast.jsx
@@ -4,7 +4,9 @@ import { Button } from "./button";
 import { useDispatch, useSelector } from "react-redux";
 import { updateToast } from "@/store/features/toastSlice";
 import gsap from "gsap";
+import { useWindowSize } from "react-use";
 const Toast = ({}) => {
+  const { width } = useWindowSize();
   const data = useSelector((state) => state.toast);
   const dispatch = useDispatch();
   useEffect(() => {
@@ -29,7 +31,7 @@ const Toast = ({}) => {
   }, [data?.title]);
 
   return (
-    <div className="fixed toast   top-5 sm:-bottom-52 sm:top-auto z-50 mx-auto sm:right-5 sm:mx-auto flex w-full justify-center sm:justify-end">
+    <div className="fixed toast    sm:-bottom-52 sm:top-auto z-50 mx-auto sm:right-5 sm:mx-auto flex w-full justify-center sm:justify-end">
       <div className="p-4 rounded-md bg-white drop-shadow-lg w-[90vw] sm:min-w-96 sm:w-fit border min-h-20">
         <div className="flex justify-between items-center">
           <div className="space-y-1">
@@ -38,7 +40,7 @@ const Toast = ({}) => {
               {data?.description}
             </p>
           </div>
-          <Button>undo</Button>
+          {/* <Button>undo</Button>*/}
         </div>
       </div>
     </div>
